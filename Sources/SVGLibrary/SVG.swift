72a8df0900682fe889c7a7b129a4057c78b5cafb
hhennessy20 <hhennessy20@gmail.com>
2023-02-27 23:52:51 -0500

    added drawtriangle, drawline, and init functionality

--- Sources/SVGLibrary/SVG.swift
+++ Sources/SVGLibrary/SVG.swift
@@ -9,21 +9,24 @@ import Foundation
 
 open class SVG {
     // YOUR CODE HERE
-    var svgFile = NSString()
+    var svgFile = String()
     // Initialize the SVG file with commands that will create a
     // width x height canvas
     public init(width: UInt, height: UInt) {
         // YOUR CODE HERE
-        let canvas = CGSize(width: width, height: height)
+        svgFile = svgFile + "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
+        svgFile = svgFile + "<svg version=\"1.1\" baseProfile=\"full\" width=\"" + width + "\" height=\"" + height + "\" xmlns=\"http://www.w3.org/2000/svg\">\n"
+
     }
     
     // YOUR CODE HERE
-    func draw_line(x1: Int,y1: Int, x2: Int, y2: Int, color: String) {
-        contents_add("<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke=\"%s\" />\n", x1, y1, x2, y2, color);
+    func draw_line(_ x1: Int, _ y1: Int, _ x2: Int, _ y2: Int, _ color: String) {
+        svgFile = svgFile + "<line x1=\"" + x1 + "\" y1=\"" + y1 + "\" x2=\"" + x2 + "\" y2=\"" + y2 + "\" stroke=\"" + color + "\" />\n"
     }
     // Write the SVG file to disk
     public func write(filePath: String) {
         // YOUR CODE HERE
+        svgFile = svgFile + "</svg>\n"
         svgFile.write(toFile: filePath, atomically: true, encoding: 4)
     }
 }
